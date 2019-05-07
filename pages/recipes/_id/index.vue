582a49290eabe99f1d706d293db5f3626418fc77
sjay <sewejed@gmail.com>
2019-05-07 22:13:52 +0100

    first commit

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="single-recipe">
+    <h1>{{ recipe.title }}</h1>
+    <div>
+      <img class="recipe-image" :src="recipe.thumbnail" alt="">
+    </div>
+    <p>The recipe description</p>
+  </section>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipe: [
+            {
+              id: "food-one",
+              thumbnail: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/07/15/chinese.jpg",
+              title: "Delecious Food",
+              previewText: "Cool food for the body"
+            },
+            {
+              id: "food-two",
+              thumbnail: "https://assets3.thrillist.com/v1/image/2815308/size/tmg-article_default_mobile.jpg",
+              title: "Delicious chicken",
+              previewText: "Cool food for the body"
+            }
+          ].find(el => el.id === context.params.id)
+        })
+      }, 1000)
+    })
+  }
+}
+</script>
+
+
+<style scoped>
+  /* style the single-recipe class */
+  .single-recipe {
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 30px;
+  }
+
+  .recipe-image {
+    width: 100%;
+  }
+</style>
---
+++ pages/recipes/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="recipes">
+    <Recipe 
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      :thumbnail="recipe.thumbnail"
+      :title="recipe.title"
+      :id="recipe.id"
+      :previewText="recipe.previewText"
+    />
+  </section>
+</template>
+
+<script>
+
+import Recipe from '@/components/Recipe'
+ 
+export default {
+  components: {
+    Recipe
+  },
+  asyncData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipes: [
+            {
+              id: "food-one",
+              thumbnail: "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/02/07/15/chinese.jpg",
+              title: "Delecious Food",
+              previewText: "Cool food for the body"
+            },
+            {
+              id: "food-two",
+              thumbnail: "https://assets3.thrillist.com/v1/image/2815308/size/tmg-article_default_mobile.jpg",
+              title: "Delicious chicken",
+              previewText: "Cool food for the body"
+            }
+          ]
+        })
+      }, 1000)
+    })
+  }
+}
+</script>
+
+
+<style scoped>
+  .recipes {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+  }
+
+ 
+</style>
